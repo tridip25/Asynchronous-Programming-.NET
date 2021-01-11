447e7cf155ea7d71b0820be8902bfa1965f4ba52
tridip25 <74949104+tridip25@users.noreply.github.com>
2021-01-11 16:58:02 +0530

    Add files via upload

---
+++ ParallelForloop/ParallelForloop/data11.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelForloop
+{
+    public class data11
+    {
+    
+        public string period { get; set; }
+        public double previously { get; set; }
+        public double revised { get; set; }
+    }
+}
