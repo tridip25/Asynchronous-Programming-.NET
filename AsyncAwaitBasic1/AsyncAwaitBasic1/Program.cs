1a1d33b8f8d2897e49c8eb0b61a97d05f5589c0f
tridip25 <74949104+tridip25@users.noreply.github.com>
2021-01-11 16:50:32 +0530

    Add files via upload

---
+++ AsyncAwaitBasic1/AsyncAwaitBasic1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitBasic1
+{
+    class Program
+    {       //uncomment the commented lines to see the difference
+        static void Main(string[] args)
+        {
+            //rr();
+            //Thread.Sleep(5000);   //this is just to give a time delay
+            method1();
+            method2();
+        }
+
+        //static async Task rr()
+        //{
+        //    await method1();
+        //    method2();
+        //}
+
+        static async Task method1()
+        {
+           await Task.Run(()=>
+           {
+               for (int i = 0; i < 50; i++)
+               {
+                   Console.WriteLine($"Hello : {i}");
+               }
+           }
+           );
+        }
+
+        static void method2()
+        {
+            for(int i=0;i<100;i++)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
